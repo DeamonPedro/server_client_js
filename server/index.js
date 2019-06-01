8427e406163744244888fc429a6af72dbba38261
DaemonPedro <pedro.lucas.root@gmail.com>
2019-06-01 17:50:04 -0300

    server 0.0.1

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
-const net = require("net");
-var readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stduot    
-})
-net.createServer(function (connection){
-    module.exports.connection = connection
-}).listen(30)
+const http = require("http")
+const express = require("express")
+const debug = require("debug")
 
-readline.question(">", function (input){
-    module.exports.connection.write(input)
-    module.exports.fuct()
-}) 
+const port = 1010
+
+function onerror(error){
+    console.log(error)
+}
+
+const app = express()
+app.set("port",port)
+app.set("error",onerror)
+
+const server = http.createServer(app)
+const router = express.Router();
+
+const route = router.get("/",(req,res,next)=>{
+    res.status(200).send({
+        status:"200",
+        name: "pedro"
+    })
+})
+
+app.use("/",route)
+server.listen(port)
+console.log("[+] Server Rdunning...")
