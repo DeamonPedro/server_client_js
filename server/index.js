d71167483f715dec439aee15e15e9123168e62ba
DaemonPedro <pedro.lucas.root@gmail.com>
2019-06-03 17:32:54 -0300

    server 0.0.3

--- server/index.js
+++ server/index.js
@@ -1,15 +1,34 @@
 const http = require("http")
 const express = require("express")
-const debug = require("debug")
+const bodyParser = require('body-parser')
+//rota de login
 const login = require("./login")
-
-const port = 1010
+//rota de "registrar-se"
+const sign_up = require("./sign_up")
+//rota para validação de conta
+const confirm_account = require("./confirm_account")
+//rota de pedidos
+const request = require("./request")
+//conexão com o DB
+sql = require("./db")
 
 const app = express()
-app.set("port",port)
-const server = http.createServer(app)
 
-app.use("/",login)
+//porta do servidor
+const port = 80
+app.set("port",port)
+
+//acetar retorno en json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//define as rotas
+app.use(login)
+app.use(sign_up)
+app.use(confirm_account)
+app.use(request)
+
+const server = http.createServer(app)
 server.listen(port)
 console.log("[+] Server Running...")
 module.exports = app
--- server/login.js
+++ server/login.js
@@ -1,7 +1,29 @@
 const express = require("express")
 
-module.exports = express.Router().get("/",(req,res,next)=>{
-    res.status(200).send({
-        status:200
-    })
+logged_accounts = {}
+
+module.exports = express.Router().post("/login",(req,res,next)=>{
+    //verifica se todos os parametros (método POST) foram recebidos
+    if(req.body.email&&req.body.pass){
+        //busca por senha do email do parametro no DB
+        sql.query("SELECT pass FROM users WHERE email = '"+req.body.email+"';", function (error, results, fields){
+            if(error) return console.log(error)
+            //se o DB não retornar nada a conta não existe
+            if (results.length==0) return res.status(200).send({
+                "error":"The account does not exist"
+            })
+            if (results[0].pass == req.body.pass){
+                var user_key = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+                res.status(200).send({
+                    "key": user_key
+                })
+                logged_accounts[req.body.email] = user_key
+                return
+            }
+        })
+    }else{
+        res.status(200).send({
+            "error":"Incomplete login"
+        })
+    }
 })
