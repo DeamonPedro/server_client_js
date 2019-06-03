d685852077d6cc6a1cdd6a1cb7dd64a8d9f05705
DaemonPedro <pedro.lucas.root@gmail.com>
2019-06-03 17:34:55 -0300

    server 0.0.4

---
+++ server/request.js
@@ -0,0 +1,28 @@
+const express = require("express")
+
+module.exports = express.Router().post("/request",(req,res,next)=>{
+    //verifica a existência de todos parametros (método POST)
+    if(req.body.prod&&req.body.quant&&req.body.email&&req.body.key){
+        sql.query("SELECT * FROM users WHERE email = '"+req.body.email+"';",function (error, results, fields){
+            if(error) return res.status(200).send({
+                "error":"Error while reguesting"
+            })
+            if(results.length==0) return res.status(200).send({
+                "error":"Unregisted account"
+            })
+            if(logged_accounts[req.body.email]==req.body.key){
+                return res.status(200).send({
+                    "status":"Confirmed request"
+                })
+            }else{
+                return res.status(200).send({
+                    "error":"Invalid key"
+                })
+            }
+        })
+    }else{
+        res.status(200).send({
+            "error":"Invalid request"
+        })
+    }
+})
---
+++ server/sign_up.js
@@ -0,0 +1,43 @@
+const express = require("express")
+
+//dicionario de contas esperando validação
+no_confirmed_accounts={}
+
+module.exports = express.Router().post("/sign_up",(req,res,next)=>{
+    //verifica se todos os parametros (método POST) foram recebidos
+    if(req.body.name&&req.body.email&&req.body.pass&&req.body.lat&&req.body.lng){
+        //verifica o email atual já solicitou validação
+        for(var key in no_confirmed_accounts){
+            if(no_confirmed_accounts[key].email == req.body.email) return res.status(200).send({
+                "error":"This email is already awaiting validation"
+            })
+        }
+        //verifica se o email já foi registrado
+        sql.query("SELECT * FROM users WHERE email = '"+req.body.email+"';",function (error, results, fields){
+            if(error) return res.status(200).send({
+                "error":"Error while validating"
+            })
+            if(results.length>0) return res.status(200).send({
+                "error":"E-mail already registered"
+            })
+            //cria string aleatoria para link de validação de conta
+            var random_code = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+            //adiciona a um dicionario de contas esperando validação
+            no_confirmed_accounts[random_code+""] = {
+                name : req.body.name,
+                email : req.body.email,
+                pass : req.body.pass,
+                lat : req.body.lat,
+                lng : req.body.lng
+            }
+            res.status(200).send({
+                "status":"Waiting Validation",
+                "code":random_code+""
+            })
+        })
+    }else{
+        res.status(200).send({
+            "error":"Invalid datas"
+        })
+    }
+})
